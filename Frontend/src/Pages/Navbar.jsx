0f4b9ac9a1ac91c467451c4be7821cf0c980ef3b
Ayush Kesarwani <95116904+AKESARWANI12@users.noreply.github.com>
2024-03-25 15:50:58 +0530

    Update Navbar.jsx

--- Frontend/src/Pages/Navbar.jsx
+++ Frontend/src/Pages/Navbar.jsx
@@ -1,36 +1,37 @@
-import React from 'react';
-import { Link, useNavigate } from 'react-router-dom'; // Assuming you're using React Router v6
-
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 const Navbar = () => {
-  const navigate = useNavigate(); // Initialize useNavigate hook
-
+  const navigate = useNavigate();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   const logoutHandler = () => {
     localStorage.removeItem("userInfo");
     navigate("/");
   };
-
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <Link className="navbar-brand" to="/">Home</Link>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <button className="navbar-toggler" type="button" onClick={toggleMenu}>
         <span className="navbar-toggler-icon"></span>
       </button>
 
-      <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link className="nav-link" to="/upload">Upload</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/users">Users</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/search">Dashboard</Link>
-          </li>
-          <li className="nav-item">
-            <button className="btn btn-dark" onClick={logoutHandler}>Logout</button>
-          </li>
-        </ul>
+      <div className={`collapse navbar-collapse${isMenuOpen ? ' show' : ''}`}>    
+        <ul className="navbar-nav" style={{ marginLeft: 'auto' }}>
+      <li className="nav-item">
+    <Link className="nav-link" to="/upload" onClick={toggleMenu}>Upload</Link>
+      </li>
+      <li className="nav-item">
+    <Link className="nav-link" to="/users" onClick={toggleMenu}>Users</Link>
+      </li>
+     <li className="nav-item">
+    <Link className="nav-link" to="/search" onClick={toggleMenu}>Dashboard</Link>
+      </li>
+     <li className="nav-item">
+    <button className="btn btn-dark" onClick={logoutHandler}>Logout</button>
+     </li>
+</ul>
       </div>
     </nav>
   );
