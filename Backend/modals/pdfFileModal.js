95253ec144da99cf7a235c999da9bef5d2a8fae8
Ayush Kesarwani <95116904+AKESARWANI12@users.noreply.github.com>
2024-03-26 02:53:47 +0530

    Update pdfFileModal.js

--- Backend/modals/pdfFileModal.js
+++ Backend/modals/pdfFileModal.js
@@ -6,7 +6,7 @@ const pdfFileSchema = new mongoose.Schema({
     file: { type: String, required: true }, // Assuming file paths are stored
     uploaded_at: { type: Date, default: Date.now },
     uploaded_by: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    
+    filecontent:{type:String},
 });
 
  const PDFFile= mongoose.model('PDFFile', pdfFileSchema);
