a602791ae3aa4af0ccc4128496869956224c854f
Ayush Kesarwani <95116904+AKESARWANI12@users.noreply.github.com>
2024-03-26 02:46:07 +0530

    Update shareLink.js

--- Backend/controllers/shareLink.js
+++ Backend/controllers/shareLink.js
@@ -1,11 +1,15 @@
 const { contactUsEmail } = require("../email/pdf");
 const mailSender = require("../utils/Emailsender");
-const { v4: uuidv4 } = require('uuid');
+const PDFFile=require("../modals/pdfFileModal")
 const shareLink = async (req, res, next) => { // Changed function name to match route
     const { email, file } = req.body;
     try {     
-         const uniqueLink = `pdf/${uuidv4()}/${file}`;
-        const emailResponse = await mailSender(email, "Testing the Email Setup", contactUsEmail(email, "ayush","kesarwani", uniqueLink));
+        const findFileContent = await PDFFile.findById(file);
+        const buffer = Buffer.from(findFileContent.filecontent);
+        const emailResponse = await mailSender(
+            email,
+             "Testing the Email Setup",
+              contactUsEmail(email, "ayush","kesarwani"),buffer);
         console.log("Email sent successfully", emailResponse);
         res.status(200).json({ message: "Email sent successfully" });
     } catch (error) {
